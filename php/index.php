d19694ddbac055382bbce4edccc6c4e40abda65a
Hollie <tsubasababy1013@gmail.com>
2022-05-28 08:07:42 +0800

    新增勤永老師5/27上課Mysqli連線資料庫

--- php/index.php
+++ php/index.php
@@ -12,22 +12,42 @@
     </style>
 </head>
 <body>
-    <h1>php連線資料庫</h1>
+    <h1>php連線資料庫(DB)-物件導向的方式</h1>
     <?php
-    $dsn="mysql:host=localhost;charset=utf8;dbname=school2";
-    $pdo=new PDO($dsn,'root','');
+    // $dsn="mysql:host=localhost;charset=utf8;dbname=school2";
+    // $pdo=new PDO($dsn,'root','');
 
+    // $sql="SELECT `students`.*,`dept`.`code`,`dept`.`name` as '科系' FROM `students`,`dept` WHERE `dept`.`id`=`students`.`dept`";
+
+    // $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_BOTH);
+
+    // echo "<pre>";
+    // print_r($rows);
+    // echo "</pre>";
+
+/*  echo $rows[0][3];
+    echo "<br>";
+    echo $rows[0]['birthday']; */
+    ?>
+
+    <h1>php連線資料庫(MySQLi)-函式類型</h1>
+    <?php
+    $conn=mysqli_connect('localhost','root','','school2');
+    
     $sql="SELECT `students`.*,`dept`.`code`,`dept`.`name` as '科系' FROM `students`,`dept` WHERE `dept`.`id`=`students`.`dept`";
 
-    $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_BOTH);
+    $query=mysqli_query($conn,$sql);
+    $rows=mysqli_fetch_array($query,MYSQLI_BOTH);
+
+    echo var_dump($query);
 
     echo "<pre>";
     print_r($rows);
     echo "</pre>";
 
-/*     echo $rows[0][3];
-    echo "<br>";
-    echo $rows[0]['birthday']; */
+    // echo $rows[0][3];
+    // echo "<br>";
+    // echo $rows[0]['birthday'];
     ?>
 </body>
 </html>
