04600761731f03ea148cb1f9b74e8652b03fdb22
Hollie <tsubasababy1013@gmail.com>
2022-05-28 11:36:26 +0800

    使用foreach撈資料

--- php/index.php
+++ php/index.php
@@ -9,6 +9,29 @@
         h1,h2,h3,h4{
             text-align: center;
         }
+
+        table{
+            border-collapse:collapse;
+            border: 3px solid blue;
+            margin: auto;
+        }
+
+        table td{
+            padding: 0.5rem;
+            border: 1px solid #aaa;
+        }
+
+        table tr:nth-child(odd){
+            background: lightgreen;
+        }
+
+        table tr:nth-child(even){
+            background: lightcyan;
+        }
+
+        table tr:hover{
+            background: lightcoral;
+        }
     </style>
 </head>
 <body>
@@ -59,23 +82,45 @@
 
     <h1>php連線資料庫(MySQLi)-函式類型-迴圈撈資料</h1>  
     <?php
-    $conn=mysqli_connect('localhost','root','','school2');
+    // $conn=mysqli_connect('localhost','root','','school2');
     
-    $sql="SELECT `students`.*,`dept`.`code`,`dept`.`name` as '科系' FROM `students`,`dept` WHERE `dept`.`id`=`students`.`dept`";
+    // $sql="SELECT `students`.*,`dept`.`code`,`dept`.`name` as '科系' FROM `students`,`dept` WHERE `dept`.`id`=`students`.`dept`";
 
-    $query=mysqli_query($conn,$sql);
+    // $query=mysqli_query($conn,$sql);
     
-    $count=0;
-    while($row=mysqli_fetch_array($query,MYSQLI_BOTH)){
-        if($row['name']=='王鳳如'){
-            echo "<pre>";
-            print_r($row);
-            echo "</pre>";
+    // $count=0;
+    // while($row=mysqli_fetch_array($query,MYSQLI_BOTH)){
+    //     if($row['name']=='王鳳如'){
+    //         echo "<pre>";
+    //         print_r($row);
+    //         echo "</pre>";
+    //     }
+    //     $count++;
+    // }
+    // echo $count;
+    ?>  
+    
+    <h1>使用foreach的方式撈資料</h1>
+    <?php
+        $dsn="mysql:host=localhost;charset=utf8;dbname=school2";
+        $pdo=new PDO($dsn,'root','');
+    
+        $sql="SELECT `students`.*,`dept`.`code`,`dept`.`name` as '科系' FROM `students`,`dept` WHERE `dept`.`id`=`students`.`dept`";
+    
+        $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+        echo"<table>";
+        foreach($rows as $key => $row){
+            echo "<tr>";
+                echo "<td>{$row['id']}</td>";
+                echo "<td>{$row['school_num']}</td>";
+                echo "<td>{$row['name']}</td>";
+                echo "<td>{$row['科系']}</td>";
+                echo "<td>{$row['parents']}</td>";
+            echo "</tr>";
         }
-        $count++;
-    }
-    echo $count;
-    ?>     
+        echo"</table>";
+    ?>
 
 </body>
 </html>
