24881dd62b20245de857e0ed99db484ca7e31016
Hollie <tsubasababy1013@gmail.com>
2022-05-30 12:01:09 +0800

    建立編輯用表單及每筆資料傳輸id值的機制

---
+++ 20220530/edit.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>編輯學生資料</title>
+    <style>
+        *{
+            color: indigo;
+        }
+        body{
+            margin: 10px;
+            padding: 20px;
+            border: 1px solid indigo;
+            border-radius: 5px;
+        }
+
+        label{
+            display: block;
+            padding: 10px;
+        }
+
+        label input{
+            padding: 3px;
+            font-size: 1.2rem;
+        }
+
+        input{
+            border: 1px solid indigo;
+            border-radius: 30px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>編輯學生資料</h1>
+    <?php
+    
+    $id=$_GET['id'];
+    echo "要編輯的是id為".$id."的資料";
+    
+    ?>
+    <form action="update.php" method="post">
+        <label for="">學號：<input type="text" name="uni_id" id="uni_id"></label>
+        <label for="">班級座號：<input type="text" name="seat_num" id="seat_num"></label>
+        <label for="">姓名：<input type="text" name="name" id="name"></label>
+        <label for="">生日：<input type="text" name="birthday" id="birthday"></label>
+        <label for="">身分證號碼：<input type="text" name="national_id" id="national_id"></label>
+        <label for="">住址：<input type="text" name="address" id="address"></label>
+        <label for="">家長：<input type="text" name="parent" id="parent"></label>
+        <label for="">電話：<input type="text" name="telphone" id="telphone"></label>
+        <label for="">科別：<input type="text" name="major" id="major"></label>
+        <label for="">畢業國中：<input type="text" name="secondary" id="secondary"></label>
+        <br>
+        <input type="hidden" name="id" value="<?=$id;?>">
+        <input type="submit" value="更新">
+        <input type="reset" value="重置">
+    </form>
+</body>
+
+</html>
