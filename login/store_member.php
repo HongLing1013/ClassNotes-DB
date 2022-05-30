ba3e6b6c61375a99197ac71c462b32b103b7f139
Hollie <tsubasababy1013@gmail.com>
2022-05-30 17:34:57 +0800

    新增資料庫檔案

--- login/store_member.php
+++ login/store_member.php
@@ -2,6 +2,11 @@
 
 $dsn="mysql:host=localhost;charset=utf8;dbname=member";
 $pdo=new PDO($dsn,'root','');
+
+// 正是做法的話
+// $acc=$_POST['acc'];
+// $acc=chkAcc($acc);
+
 $sql="INSERT INTO `users` (`acc`,`pw`,`birthday`,`passnote`,`email`) 
                   values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['birthday']}','{$_POST['passnote']}','{$_POST['email']}');";
 
